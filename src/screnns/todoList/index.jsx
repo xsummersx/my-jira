35501e3bda1cc7c046d28e9040558988b3c8a67f
xsummersx <605369747@qq.com>
2022-01-26 10:31:00 +0800

    添加hook

--- src/screnns/todoList/index.jsx
+++ src/screnns/todoList/index.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-24 19:49:47
- * @LastEditTime: 2022-01-24 21:31:56
+ * @LastEditTime: 2022-01-26 10:17:53
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \my-jira\src\screnns\todoList\index.jsx
@@ -11,7 +11,7 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Search } from "./search";
 import { List } from "./list";
 import * as qs from "qs";
-import { clearObject } from "utils/index";
+import { clearObject, useDebounce } from "utils/index";
 const apiUrl = process.env.REACT_APP_API_URL;
 export const TodoList = () => {
   const [peram, setPeram] = useState({
@@ -20,11 +20,14 @@ export const TodoList = () => {
   });
   const [users, setUser] = useState([]);
   const [prolist, setprolist] = useState([]);
+  const debounceParam = useDebounce(peram, 2000);
   useEffect(() => {
-    fetch(`${apiUrl}/proList?${qs.stringify(clearObject(peram))}`).then(async (response) => {
-      setprolist(await response.json());
-    });
-  }, [peram]);
+    fetch(`${apiUrl}/proList?${qs.stringify(clearObject(debounceParam))}`).then(
+      async (response) => {
+        setprolist(await response.json());
+      },
+    );
+  }, [debounceParam]);
   useEffect(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       setUser(await response.json());
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2022-01-24 21:19:16
- * @LastEditTime: 2022-01-24 21:23:09
+ * @LastEditTime: 2022-01-26 10:26:44
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \my-jira\src\utils\index.js
  */
+import { useState, useEffect } from "react";
 const isFalse = (val) => {
   return val === 0 ? false : !val;
 };
@@ -18,4 +19,29 @@ export const clearObject = (object) => {
     }
   });
   return obj;
+};
+
+const debounce = (func, delay) => {
+  let timeout;
+  return () => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => func, delay);
+  };
+};
+
+export const useDebounce = (value, delay) => {
+  const [peramValue, setPeramValue] = useState(value);
+  useEffect(() => {
+    //每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => {
+      setPeramValue(value);
+    }, delay);
+    //每次在上一个UseEffect处理完之后再运行
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+  return peramValue;
 };
